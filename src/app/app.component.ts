cf097dded173c1eb1bf4a4031613e1caf936268a
Mikhail Tsai <tsaimikhail@gmail.com>
2024-05-02 03:30:07 +0700

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NgDompurifySanitizer} from "@tinkoff/ng-dompurify";
+import {
+  TUI_SANITIZER, TuiModeModule, TuiRootModule, TuiThemeNightModule,
+} from "@taiga-ui/core";
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {
+  TuiAppBarModule, TuiAvatarModule, TuiButtonModule,
+  TuiCardModule,
+  TuiHeaderModule,
+  TuiSurfaceModule,
+  TuiTitleModule
+} from "@taiga-ui/experimental";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TuiThemeNightModule,
+    TuiModeModule,
+    TuiRootModule,
+    TuiAppBarModule,
+    TuiCardModule,
+    TuiSurfaceModule,
+    TuiHeaderModule,
+    TuiTitleModule,
+    TuiAvatarModule,
+    TuiButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
+  providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'portfolio';
